3ea7d8d3995fdd5a88078fe7e8a9e1161cb87fcc
Ponokefalos <pesrekaisi@gmail.com>
2014-02-01 18:23:29 +0200

    icons deleted

--- Order@Samos/gen/com/tardis/ordersamos/R.java
+++ Order@Samos/gen/com/tardis/ordersamos/R.java
@@ -24,17 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int main=0x7f020001;
         public static final int red=0x7f020002;
+        public static final int restaurant_logo=0x7f020003;
     }
     public static final class id {
+        public static final int ImageButton01=0x7f080002;
         public static final int RelativeLayout1=0x7f080001;
-        public static final int action_settings=0x7f080008;
-        public static final int imageButton1=0x7f080004;
-        public static final int imageButton2=0x7f080002;
-        public static final int imageButton3=0x7f080003;
-        public static final int imageButton4=0x7f080005;
-        public static final int imageButton5=0x7f080006;
-        public static final int imageButton6=0x7f080007;
+        public static final int action_settings=0x7f080006;
+        public static final int imageButton1=0x7f080003;
         public static final int scrollView1=0x7f080000;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main_menu=0x7f030000;
--- Order@Samos/src/com/tardis/ordersamos/MainMenu.java
+++ Order@Samos/src/com/tardis/ordersamos/MainMenu.java
@@ -16,11 +16,6 @@ public class MainMenu extends Activity {
 	//	actionbar.setTitle("kala eimai");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_menu, menu);
-		return true;
-	}
+	
 
 }
