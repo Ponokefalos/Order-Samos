34d05bc1d5163d02253f25d42930d0c7d8f90f65
Ponokefalos <pesrekaisi@gmail.com>
2014-02-10 01:22:12 +0200

    data in fagita.xml 2 eikonidia EksigiseisAurioPLZ
    mpikan ola ta  dedomena twn katalogwn sto fagita.xml prosthesa kai ta
    vakxos, sweet na ta sto drawble mono

--- Order@Samos/gen/com/tardis/ordersamos/R.java
+++ Order@Samos/gen/com/tardis/ordersamos/R.java
@@ -9,36 +9,190 @@ package com.tardis.ordersamos;
 
 public final class R {
     public static final class array {
-        public static final int Banvalues=0x7f07001d;
-        public static final int Estiatoria=0x7f07001c;
-        public static final int Evrys_burger=0x7f07000c;
-        public static final int Evrys_burger_times=0x7f07000d;
-        public static final int Evrys_omeletes=0x7f070008;
-        public static final int Evrys_omeletes_times=0x7f070009;
-        public static final int Evrys_orektika=0x7f070002;
-        public static final int Evrys_orektika_times=0x7f070003;
-        public static final int Evrys_piata=0x7f070006;
-        public static final int Evrys_piata_times=0x7f070007;
-        public static final int Evrys_pites=0x7f070004;
-        public static final int Evrys_pites_times=0x7f070005;
-        public static final int Evrys_pitses_mikres_megales=0x7f070010;
-        public static final int Evrys_pitses_mikres_megales_times=0x7f070011;
-        public static final int Evrys_salates=0x7f070000;
-        public static final int Evrys_salates_times=0x7f070001;
-        public static final int Evrys_submarine_sandwich=0x7f07000e;
-        public static final int Evrys_submarine_sandwitch_times=0x7f07000f;
-        public static final int Evrys_zumarika=0x7f07000a;
-        public static final int Evrys_zumarika_times=0x7f07000b;
-        public static final int Til_Evrys=0x7f070012;
-        public static final int Til_Fame=0x7f070013;
-        public static final int Til_Giro=0x7f070015;
-        public static final int Til_Koutala=0x7f070014;
-        public static final int Til_Kouzina=0x7f070019;
-        public static final int Til_Megaro=0x7f070017;
-        public static final int Til_Nostos=0x7f070016;
-        public static final int Til_SweetnSalty=0x7f07001b;
-        public static final int Til_Taz=0x7f070018;
-        public static final int Til_Vakxos=0x7f07001a;
+        public static final int Bakxos_burger=0x7f06000e;
+        public static final int Bakxos_burger_times=0x7f06000f;
+        public static final int Bakxos_mageireuta=0x7f060008;
+        public static final int Bakxos_mageireuta_times=0x7f060009;
+        public static final int Bakxos_merides=0x7f06000a;
+        public static final int Bakxos_merides_times=0x7f06000b;
+        /**  //////////////////////////////// BAKXOS////////////////////// 
+         */
+        public static final int Bakxos_orektika=0x7f060000;
+        public static final int Bakxos_orektika_times=0x7f060001;
+        public static final int Bakxos_pites=0x7f06000c;
+        public static final int Bakxos_pites_times=0x7f06000d;
+        public static final int Bakxos_salates=0x7f060002;
+        public static final int Bakxos_salates_times=0x7f060003;
+        public static final int Bakxos_sandwich=0x7f060004;
+        public static final int Bakxos_sandwich_times=0x7f060005;
+        public static final int Bakxos_souvlista=0x7f060006;
+        public static final int Bakxos_souvlista_times=0x7f060007;
+        public static final int Banvalues=0x7f0600a5;
+        public static final int Estiatoria=0x7f0600a4;
+        public static final int Fame_burger=0x7f060016;
+        public static final int Fame_burger_times=0x7f060017;
+        public static final int Fame_krepes_almures=0x7f06002a;
+        public static final int Fame_krepes_almures_times=0x7f06002b;
+        public static final int Fame_krepes_glukes=0x7f060028;
+        public static final int Fame_krepes_glukes_times=0x7f060029;
+        public static final int Fame_merides=0x7f06001e;
+        public static final int Fame_merides_times=0x7f06001f;
+        public static final int Fame_omeletes=0x7f060014;
+        public static final int Fame_omeletes_times=0x7f060015;
+        public static final int Fame_orektika=0x7f060012;
+        public static final int Fame_orektika_times=0x7f060013;
+        public static final int Fame_pites=0x7f060022;
+        public static final int Fame_pites_times=0x7f060023;
+        public static final int Fame_pitses=0x7f06001c;
+        public static final int Fame_pitses_times=0x7f06001d;
+        /**  //////////////////////////////// FAME////////////////////// 
+         */
+        public static final int Fame_salates=0x7f060010;
+        public static final int Fame_salates_times=0x7f060011;
+        public static final int Fame_sandwich=0x7f060018;
+        public static final int Fame_sandwich_times=0x7f060019;
+        public static final int Fame_spesialite=0x7f060020;
+        public static final int Fame_spesialite_times=0x7f060021;
+        public static final int Fame_vafles=0x7f06002c;
+        public static final int Fame_vafles_times=0x7f06002d;
+        public static final int Fame_xwris_pita=0x7f060024;
+        public static final int Fame_xwris_pita_times=0x7f060025;
+        public static final int Fame_yhta=0x7f060026;
+        public static final int Fame_yhta_times=0x7f060027;
+        public static final int Fame_zumarika=0x7f06001a;
+        public static final int Fame_zumarika_times=0x7f06001b;
+        public static final int Guro_Guro_oloi_merides=0x7f060036;
+        public static final int Guro_Guro_oloi_merides_times=0x7f060037;
+        public static final int Guro_Guro_oloi_orektika=0x7f060030;
+        public static final int Guro_Guro_oloi_orektika_times=0x7f060031;
+        public static final int Guro_Guro_oloi_pites=0x7f060032;
+        public static final int Guro_Guro_oloi_pites_times=0x7f060033;
+        public static final int Guro_Guro_oloi_poikilies=0x7f060038;
+        public static final int Guro_Guro_oloi_poikilies_times=0x7f060039;
+        /**  //////////////////////////////// GIRO GIRO OLOI////////////////////// 
+         */
+        public static final int Guro_Guro_oloi_salates=0x7f06002e;
+        public static final int Guro_Guro_oloi_salates_times=0x7f06002f;
+        public static final int Guro_Guro_oloi_souvles=0x7f060034;
+        public static final int Guro_Guro_oloi_souvles_times=0x7f060035;
+        public static final int Koutala_club_sandwich=0x7f060048;
+        public static final int Koutala_club_sandwich_times=0x7f060049;
+        public static final int Koutala_mezedes=0x7f060040;
+        public static final int Koutala_mezedes_times=0x7f060041;
+        public static final int Koutala_orektika=0x7f06003c;
+        public static final int Koutala_orektika_times=0x7f06003d;
+        public static final int Koutala_pizza=0x7f06004a;
+        public static final int Koutala_pizza_times=0x7f06004b;
+        /**  //////////////////////////////// KOUTALA////////////////////// 
+         */
+        public static final int Koutala_salates=0x7f06003a;
+        public static final int Koutala_salates_times=0x7f06003b;
+        public static final int Koutala_snitsel=0x7f060044;
+        public static final int Koutala_snitsel_times=0x7f060045;
+        public static final int Koutala_souvlaki=0x7f060046;
+        public static final int Koutala_souvlaki_times=0x7f060047;
+        public static final int Koutala_yarika=0x7f06003e;
+        public static final int Koutala_yarika_times=0x7f06003f;
+        public static final int Koutala_zumarika=0x7f060042;
+        public static final int Koutala_zumarika_times=0x7f060043;
+        public static final int Kouzina_ths_mamas_aloifes=0x7f06004e;
+        public static final int Kouzina_ths_mamas_aloifes_times=0x7f06004f;
+        public static final int Kouzina_ths_mamas_burger=0x7f060062;
+        public static final int Kouzina_ths_mamas_burger_times=0x7f060063;
+        public static final int Kouzina_ths_mamas_kupriakes_pites=0x7f06005e;
+        public static final int Kouzina_ths_mamas_kupriakes_pites_times=0x7f06005f;
+        public static final int Kouzina_ths_mamas_mageireuta=0x7f060054;
+        public static final int Kouzina_ths_mamas_mageireuta_times=0x7f060055;
+        public static final int Kouzina_ths_mamas_orektika=0x7f060050;
+        public static final int Kouzina_ths_mamas_orektika_times=0x7f060051;
+        public static final int Kouzina_ths_mamas_pites=0x7f06005c;
+        public static final int Kouzina_ths_mamas_pites_times=0x7f06005d;
+        public static final int Kouzina_ths_mamas_pitses=0x7f060064;
+        public static final int Kouzina_ths_mamas_pitses_times=0x7f060065;
+        /**  //////////////////////////////// KOUZINA TIS MAMAS////////////////////// 
+         */
+        public static final int Kouzina_ths_mamas_salates=0x7f06004c;
+        public static final int Kouzina_ths_mamas_salates_times=0x7f06004d;
+        public static final int Kouzina_ths_mamas_souvles=0x7f06005a;
+        public static final int Kouzina_ths_mamas_souvles_times=0x7f06005b;
+        public static final int Kouzina_ths_mamas_thganies=0x7f060056;
+        public static final int Kouzina_ths_mamas_thganies_times=0x7f060057;
+        public static final int Kouzina_ths_mamas_yhta=0x7f060058;
+        public static final int Kouzina_ths_mamas_yhta_times=0x7f060059;
+        public static final int Kouzina_ths_mamas_ywmakia=0x7f060060;
+        public static final int Kouzina_ths_mamas_ywmakia_times=0x7f060061;
+        public static final int Kouzina_ths_mamas_zumarika=0x7f060052;
+        public static final int Kouzina_ths_mamas_zumarika_times=0x7f060053;
+        /**  //////////////////////////////// MEGARO////////////////////// 
+         */
+        public static final int Megaro_pitses=0x7f060066;
+        public static final int Megaro_pitses_times=0x7f060067;
+        public static final int Megaro_salates=0x7f060068;
+        public static final int Megaro_salates_times=0x7f060069;
+        public static final int Megaro_zumarika=0x7f06006a;
+        public static final int Megaro_zumarika_times=0x7f06006b;
+        public static final int Nostos_mageireuta=0x7f06007a;
+        public static final int Nostos_mageireuta_times=0x7f06007b;
+        public static final int Nostos_omeletes=0x7f06006e;
+        public static final int Nostos_omeletes_times=0x7f06006f;
+        /**  //////////////////////////////// NOSTOS////////////////////// 
+         */
+        public static final int Nostos_orektika=0x7f06006c;
+        public static final int Nostos_orektika_times=0x7f06006d;
+        public static final int Nostos_peinirli=0x7f060072;
+        public static final int Nostos_peinirli_times=0x7f060073;
+        public static final int Nostos_pitses=0x7f060078;
+        public static final int Nostos_pitses_times=0x7f060079;
+        public static final int Nostos_salates=0x7f060070;
+        public static final int Nostos_salates_times=0x7f060071;
+        public static final int Nostos_tortelini=0x7f060076;
+        public static final int Nostos_tortelini_times=0x7f060077;
+        public static final int Nostos_yhta=0x7f06007c;
+        public static final int Nostos_yhta_times=0x7f06007d;
+        public static final int Nostos_zumarika=0x7f060074;
+        public static final int Nostos_zumarika_times=0x7f060075;
+        public static final int Sweet_salty_burger=0x7f060082;
+        public static final int Sweet_salty_burger_times=0x7f060083;
+        public static final int Sweet_salty_club_sandwich=0x7f06008a;
+        public static final int Sweet_salty_club_sandwich_times=0x7f06008b;
+        public static final int Sweet_salty_hot_dog=0x7f060080;
+        public static final int Sweet_salty_hot_dog_times=0x7f060081;
+        /**  //////////////////////////////// SWEET & NASTY////////////////////// 
+         */
+        public static final int Sweet_salty_krepes_almures=0x7f06007e;
+        public static final int Sweet_salty_krepes_almures_times=0x7f06007f;
+        public static final int Sweet_salty_krepes_glukes=0x7f060088;
+        public static final int Sweet_salty_krepes_glukes_times=0x7f060089;
+        public static final int Sweet_salty_merides=0x7f060084;
+        public static final int Sweet_salty_merides_times=0x7f060085;
+        public static final int Sweet_salty_salates=0x7f060086;
+        public static final int Sweet_salty_salates_times=0x7f060087;
+        public static final int Tazmaniac_burger=0x7f060094;
+        public static final int Tazmaniac_burger_times=0x7f060095;
+        public static final int Tazmaniac_faghta=0x7f060098;
+        public static final int Tazmaniac_faghta_times=0x7f060099;
+        public static final int Tazmaniac_krepes_almures=0x7f060090;
+        public static final int Tazmaniac_krepes_almures_times=0x7f060091;
+        public static final int Tazmaniac_krepes_glukes=0x7f060092;
+        public static final int Tazmaniac_krepes_glukes_times=0x7f060093;
+        /**  //////////////////////////////// TAZMANIAC////////////////////// 
+         */
+        public static final int Tazmaniac_prwina=0x7f06008c;
+        public static final int Tazmaniac_prwina_times=0x7f06008d;
+        public static final int Tazmaniac_sandwich=0x7f06008e;
+        public static final int Tazmaniac_sandwich_times=0x7f06008f;
+        public static final int Tazmaniac_steak_sandwich=0x7f060096;
+        public static final int Tazmaniac_steak_sandwich_times=0x7f060097;
+        public static final int Til_Evrys=0x7f06009a;
+        public static final int Til_Fame=0x7f06009b;
+        public static final int Til_Giro=0x7f06009d;
+        public static final int Til_Koutala=0x7f06009c;
+        public static final int Til_Kouzina=0x7f0600a1;
+        public static final int Til_Megaro=0x7f06009f;
+        public static final int Til_Nostos=0x7f06009e;
+        public static final int Til_SweetnSalty=0x7f0600a3;
+        public static final int Til_Taz=0x7f0600a0;
+        public static final int Til_Vakxos=0x7f0600a2;
     }
     public static final class attr {
     }
@@ -112,21 +266,23 @@ public final class R {
         public static final int ratings=0x7f020038;
         public static final int red=0x7f020039;
         public static final int restaurant_blue=0x7f02003a;
-        public static final int sweet1=0x7f02003b;
-        public static final int sweet2=0x7f02003c;
-        public static final int sweet3=0x7f02003d;
-        public static final int sweet4=0x7f02003e;
-        public static final int sweet5=0x7f02003f;
-        public static final int sweet6=0x7f020040;
-        public static final int taz=0x7f020041;
-        public static final int tazmaniac1=0x7f020042;
-        public static final int tazmaniac2=0x7f020043;
-        public static final int tick=0x7f020044;
-        public static final int vakxos1=0x7f020045;
-        public static final int vakxos2=0x7f020046;
-        public static final int vakxos3=0x7f020047;
-        public static final int vakxos4=0x7f020048;
-        public static final int vakxos5=0x7f020049;
+        public static final int sweet=0x7f02003b;
+        public static final int sweet1=0x7f02003c;
+        public static final int sweet2=0x7f02003d;
+        public static final int sweet3=0x7f02003e;
+        public static final int sweet4=0x7f02003f;
+        public static final int sweet5=0x7f020040;
+        public static final int sweet6=0x7f020041;
+        public static final int taz=0x7f020042;
+        public static final int tazmaniac1=0x7f020043;
+        public static final int tazmaniac2=0x7f020044;
+        public static final int tick=0x7f020045;
+        public static final int vakxos=0x7f020046;
+        public static final int vakxos1=0x7f020047;
+        public static final int vakxos2=0x7f020048;
+        public static final int vakxos3=0x7f020049;
+        public static final int vakxos4=0x7f02004a;
+        public static final int vakxos5=0x7f02004b;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f0a0016;
@@ -190,25 +346,25 @@ public final class R {
         public static final int ratings=0x7f090007;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
-        public static final int prosforaEvrys=0x7f060011;
-        public static final int prosforaFame=0x7f06000d;
-        public static final int prosforaMegaro=0x7f06000e;
-        public static final int prosforaSweet=0x7f06000f;
-        public static final int prosforaTaz=0x7f060010;
-        public static final int random_fagito=0x7f06000b;
-        public static final int random_magazi=0x7f06000a;
-        public static final int title_Random_Choice=0x7f060009;
-        public static final int title_activity_ban_list=0x7f060005;
-        public static final int title_activity_estiatoria=0x7f060003;
-        public static final int title_activity_katigories=0x7f060004;
-        public static final int title_activity_prosfores=0x7f060007;
-        public static final int title_activity_random_choice=0x7f060006;
-        public static final int title_activity_ratings=0x7f060008;
-        public static final int title_curl_view=0x7f060012;
-        public static final int title_prosfores=0x7f06000c;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
+        public static final int prosforaEvrys=0x7f070011;
+        public static final int prosforaFame=0x7f07000d;
+        public static final int prosforaMegaro=0x7f07000e;
+        public static final int prosforaSweet=0x7f07000f;
+        public static final int prosforaTaz=0x7f070010;
+        public static final int random_fagito=0x7f07000b;
+        public static final int random_magazi=0x7f07000a;
+        public static final int title_Random_Choice=0x7f070009;
+        public static final int title_activity_ban_list=0x7f070005;
+        public static final int title_activity_estiatoria=0x7f070003;
+        public static final int title_activity_katigories=0x7f070004;
+        public static final int title_activity_prosfores=0x7f070007;
+        public static final int title_activity_random_choice=0x7f070006;
+        public static final int title_activity_ratings=0x7f070008;
+        public static final int title_curl_view=0x7f070012;
+        public static final int title_prosfores=0x7f07000c;
     }
     public static final class style {
         /** 
